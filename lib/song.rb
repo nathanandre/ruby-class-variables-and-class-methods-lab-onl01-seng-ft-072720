ec633d17c041eb24d3ddc19307b57c48bf3339dc
Nathan Burns <nathanandreburns@gmail.com>
2020-08-08 20:15:37 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@artists = []
+  @@genres = []
+  @@count = 0
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << @artist
+    @@genres << @genre
+  end
+  def self.count
+    @@count
+  end
+  def self.artists
+  end
+end

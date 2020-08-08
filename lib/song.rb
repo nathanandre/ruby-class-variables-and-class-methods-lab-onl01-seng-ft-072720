1ddf76b386b1d9001a3422732dbe69078d78ae83
Nathan Burns <nathanandreburns@gmail.com>
2020-08-08 22:16:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
       if artist_count[artist_name]
         artist_count[artist_name] += 1
       else
-        artist_count[genre_name] = 1
+        artist_count[artist_name] = 1
       end
     end
     genre_count

da30c5e885cdfe00d2d486c75ed47dd73edddf8d
Nathan Burns <nathanandreburns@gmail.com>
2020-08-08 21:29:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,11 +23,4 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      if genre_count[genre] 
-        genre_count[genre] += 1
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
 end
